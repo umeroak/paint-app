56efe0170b0b6e7efa3ee8f7e9c4d64a129ceb09
Kmaihaa <140195484+Kmaihaa@users.noreply.github.com>
2024-03-26 17:16:10 +0000

    crayon and spray paint fix but possible memory leak

--- Brush.java
+++ Brush.java
@@ -1,4 +1,8 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import java.util.Random;
 
 public class Brush {
@@ -14,7 +18,9 @@ public class Brush {
 
     private BrushType brushType;
     private int size;
-    private Color color;
+    private static final long FIXED_SEED = 12345; 
+    private BufferedImage crayonTexture;
+   
    
 
 
@@ -22,6 +28,15 @@ public class Brush {
         this.brushType = brushType;
         this.size = size;
         
+
+        try {
+    FileInputStream fileInputStream = new FileInputStream("Crayon.png");
+    crayonTexture = ImageIO.read(fileInputStream);
+    fileInputStream.close(); // Close the input stream after reading the image
+} catch (IOException e) {
+    e.printStackTrace();
+}
+    
         
     }
 
@@ -38,14 +53,6 @@ public class Brush {
         size = x;
     }
 
-     public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
     public void paint(Graphics2D g2d, int x, int y, int normalx, int normaly) {
         switch (brushType) {
             case DEFAULT:
@@ -53,13 +60,11 @@ public class Brush {
             g2d.drawLine(x, y, normalx, normaly);
                 break;
             case HIGHLIGHTER:
-                g2d.setStroke(new BasicStroke(size));
                 Color highlighterColor = new Color (g2d.getColor().getRed(), g2d.getColor().getGreen(), g2d.getColor().getBlue(), 20);
                 g2d.setColor(highlighterColor);
                 g2d.drawLine(x - size / 2, y - size / 2, normalx, normaly);
                 break;
             case MARKER:
-                g2d.setStroke(new BasicStroke(size));
                 int markerSize = size * 2; 
                 Color markerColor = new Color(g2d.getColor().getRed(), g2d.getColor().getGreen(), g2d.getColor().getBlue(), 100); 
                 g2d.setColor(markerColor);
@@ -67,61 +72,30 @@ public class Brush {
                 
                 break;
             case PENCIL:
-            g2d.setStroke(new BasicStroke(size));
             g2d.setColor(g2d.getColor());
         
             g2d.drawLine(x, y, x, y);
                 break;
             case PEN:
-            g2d.setStroke(new BasicStroke(size));
             g2d.setColor(g2d.getColor());
                 g2d.drawLine(x - size / 2, y - size / 2, normalx, normaly); 
                 break;
-            case CRAYON:
-                g2d.setStroke(new BasicStroke(size));
-                g2d.setColor(g2d.getColor());
-                Random random = new Random();
-    
-                // Set parameters for crayon strokes
-                int strokeLength = size; // Length of each stroke
-                int gapLength = size / 2; // Gap between strokes
-    
-                // Simulate crayon strokes with irregularity
-                int currentX = normalx;
-                int currentY = normaly;
-    
-                while (strokeLength > 0) {
-                    // Randomize the stroke length and gap length slightly
-                    int actualStrokeLength = strokeLength + random.nextInt(size) - size / 2;
-                    int actualGapLength = gapLength + random.nextInt(size) - size / 2;
-    
-                    // Calculate end point of the stroke
-                    int endX = currentX + (int) ((actualStrokeLength + random.nextInt(size) - size / 2) * Math.cos(Math.toRadians(random.nextInt(360))));
-                    int endY = currentY + (int) ((actualStrokeLength + random.nextInt(size) - size / 2) * Math.sin(Math.toRadians(random.nextInt(360))));
-    
-
-
-                    
-                    g2d.drawLine(currentX, currentY, x, y);
-    
-                    // Update current position
-                    currentX = endX + (int) ((actualGapLength + random.nextInt(size) - size / 2) * Math.cos(Math.toRadians(random.nextInt(360))));
-                    currentY = endY + (int) ((actualGapLength + random.nextInt(size) - size / 2) * Math.sin(Math.toRadians(random.nextInt(360))));
-    
-                    // Decrease remaining stroke length
-                    strokeLength -= actualStrokeLength + actualGapLength;
-
-                }
-                break;
-            case SPRAY_PAINT:
-                Random random1 = new Random();
-                int spraySize = size * 3; // Adjust spray size as needed
-                for (int i = 0; i < spraySize; i++) {
-                    int offsetX = random1.nextInt(size * 2) - size;
-                    int offsetY = random1.nextInt(size * 2) - size;
-                    g2d.fillRect(x + offsetX, y + offsetY, 1, 1);
-                }
-                break;
+        case CRAYON:
+        TexturePaint texturePaint = new TexturePaint(crayonTexture, new Rectangle(0, 0, crayonTexture.getWidth(), crayonTexture.getHeight()));
+        g2d.setPaint(texturePaint);
+        g2d.setStroke(new BasicStroke(size, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        g2d.drawLine(x, y, normalx, normaly);
+    break;
+        
+    case SPRAY_PAINT:
+        Random randomSpray = new Random(FIXED_SEED); 
+        int spraySize = size * 8; 
+        for (int i = 0; i < spraySize; i++) {
+            int offsetX = randomSpray.nextInt(size * 2) - size;
+            int offsetY = randomSpray.nextInt(size * 2) - size;
+            g2d.fillRect(x + offsetX, y + offsetY, 1, 1);
+        }
+        break;
                 default:
         }
         
